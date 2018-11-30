3bab5e5ff53bb0f0dd7e9103359a796a341ca80d
ryan-baxter <45466773+ryan-baxter@users.noreply.github.com>
2018-11-30 21:54:03 +0000

    Add files via upload

---
+++ Room2/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Room2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblMeasure = new System.Windows.Forms.Label();
+            this.cboMeasure = new System.Windows.Forms.ComboBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblMeasure);
+            this.groupBox1.Controls.Add(this.cboMeasure);
+            this.groupBox1.Controls.Add(this.txtLength);
+            this.groupBox1.Controls.Add(this.txtWidth);
+            this.groupBox1.Controls.Add(this.txtHeight);
+            this.groupBox1.Controls.Add(this.btnSubmit);
+            this.groupBox1.Controls.Add(this.lblHeight);
+            this.groupBox1.Controls.Add(this.lblWidth);
+            this.groupBox1.Controls.Add(this.lblLength);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(348, 105);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Measurements of the Room";
+            // 
+            // lblMeasure
+            // 
+            this.lblMeasure.AutoSize = true;
+            this.lblMeasure.Location = new System.Drawing.Point(178, 48);
+            this.lblMeasure.Name = "lblMeasure";
+            this.lblMeasure.Size = new System.Drawing.Size(74, 13);
+            this.lblMeasure.TabIndex = 10;
+            this.lblMeasure.Text = "Measurement:";
+            // 
+            // cboMeasure
+            // 
+            this.cboMeasure.FormattingEnabled = true;
+            this.cboMeasure.Items.AddRange(new object[] {
+            "Feet",
+            "Yards"});
+            this.cboMeasure.Location = new System.Drawing.Point(258, 45);
+            this.cboMeasure.Name = "cboMeasure";
+            this.cboMeasure.Size = new System.Drawing.Size(84, 21);
+            this.cboMeasure.TabIndex = 9;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(72, 19);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 6;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(242, 19);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 5;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(72, 45);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 4;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(137, 71);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(12, 48);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(41, 13);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Height:";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(187, 22);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(38, 13);
+            this.lblWidth.TabIndex = 1;
+            this.lblWidth.Text = "Width:";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(10, 22);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(43, 13);
+            this.lblLength.TabIndex = 0;
+            this.lblLength.Text = "Length:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstResults);
+            this.groupBox2.Location = new System.Drawing.Point(13, 124);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 95);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(6, 19);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(336, 69);
+            this.lstResults.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(373, 229);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Room Calculation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.ComboBox cboMeasure;
+        private System.Windows.Forms.Label lblMeasure;
+    }
+}
+
---
+++ Room2/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Room2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Validation Checks
+        private new bool Validate()
+        {
+            //Validation Variables
+            int allowedChars;
+            string lengthText = txtLength.Text;
+            string widthText = txtWidth.Text;
+            string heightText = txtHeight.Text;
+            string measureText = cboMeasure.Text;
+
+            //Length Check
+            if (string.IsNullOrEmpty(lengthText))
+            {
+                MessageBox.Show("Please enter the length of the room", "Length Error");
+                return false;
+            }
+
+            if (!int.TryParse(lengthText, out allowedChars))
+            {
+                MessageBox.Show("Please enter only numbers", "Length Error");
+                return false;
+            }
+
+            //Width Check
+            if (string.IsNullOrEmpty(widthText))
+            {
+                MessageBox.Show("Please enter the width of the room", "Width Error");
+                return false;
+            }
+
+            if (!int.TryParse(widthText, out allowedChars))
+            {
+                MessageBox.Show("Please enter only numbers", "Width Error");
+                return false;
+            }
+
+            //Height Check
+            if (string.IsNullOrEmpty(heightText))
+            {
+                MessageBox.Show("Please enter the height of the room", "Height Error");
+                return false;
+            }
+
+            if (!int.TryParse(heightText, out allowedChars))
+            {
+                MessageBox.Show("Please enter only numbers", "Height Error");
+                return false;
+            }
+
+            //Measurement Check
+            if (string.IsNullOrEmpty(measureText))
+            {
+                MessageBox.Show("Please enter the measurement of the room", "Measurement Error");
+                return false;
+            }
+
+            if (cboMeasure.Text.Contains("Feet"))
+            {
+                return true;
+            }
+            else if (cboMeasure.Text.Contains("Yards"))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Please select either Feet or Yards", "Measurement Error");
+                return false;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Clear Previous Calculations
+            lstResults.Items.Clear();
+
+            //Room Measurements & Area/Volume
+            int length;
+            int width;
+            int height;
+            int area;
+            int volume;
+
+            //To Calculate Paint
+            int paintNeeded;
+            int wallsValue;
+
+            //Calculating The Area Of The Walls
+            double wallsConverted;
+            int lengthDouble;
+            int widthDouble;
+            int walls;
+
+            //Room Measurement
+            string measure;
+
+            if(!Validate())
+            {
+                return;
+            }
+
+            //Convert Inputs
+            length = Convert.ToInt32(txtLength.Text);
+            width = Convert.ToInt32(txtWidth.Text);
+            height = Convert.ToInt32(txtHeight.Text);
+            measure = Convert.ToString(cboMeasure.SelectedItem);
+
+            //Check Whether 'Feet' Or 'Yards' Is Entered
+            string measureArea = "";
+            string measureVolume = "";
+
+            switch (measure)
+            {
+                case "Feet":
+                    measureArea = "Square Feet";
+                    measureVolume = "Cubic Feet";
+                    break;
+                case "Yards":
+                    measureArea = "Square Yards";
+                    measureVolume = "Cubic Yards";
+                    break;
+            }
+
+            //Area
+            area = length * width;
+            lstResults.Items.Add("The area of the floor is " + area + " " + measureArea);
+
+            //Volume
+            volume = length * width * height;
+            lstResults.Items.Add("The volume of the room is " + volume + " " + measureVolume);
+
+            //Area Of Walls
+            lengthDouble = length * height * 2;
+            widthDouble = width * height * 2;
+            walls = lengthDouble + widthDouble;
+            wallsConverted = 0;
+
+            //Convert Feet/Yards To Meters
+            switch (measure)
+            {
+                case "Feet":
+                    wallsConverted = walls / 10.764;
+                    break;
+                case "Yards":
+                    wallsConverted = walls / 1.1960;
+                    break;
+            }
+
+            lstResults.Items.Add("The total area of the walls is " + wallsConverted + " Meters Squared");
+
+            //Calculate Paint
+            paintNeeded = 0;
+            wallsValue = Convert.ToInt32(wallsConverted);
+
+            for (int i = 0; i < wallsValue;)
+            {
+                paintNeeded = paintNeeded + 1;
+                wallsValue = wallsValue - 10;
+            }
+
+            lstResults.Items.Add("The amount of paint needed (in Liters) is " + paintNeeded + ", " + "covering 10m²  per Liter");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
